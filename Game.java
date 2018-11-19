0d87098363d75fc157f8d79c6ce5967b4e1e46f4
JellyfishJoe <jellyfishjoe5@gmail.com>
2018-11-19 13:07:21 -0800

    asdfg

--- Game.java
+++ Game.java
@@ -3,39 +3,49 @@ import java.io.*;
 
 public class Game{
 
-	private String guessWord;
+	private String targetWord;
+	private int targetWordLength;
 
-	public void main(String targetWord){
+	public void main(String word){
 
 		List<String> words = new ArrayList<String>();
 		List<String> disallowedChars = new ArrayList<String>();
 		Game game = new Game();
 		Guesser guesser = new Guesser();
 
-		words = game.Reader("C:\\Users\\etone\\Desktop\\javaProjects\\hangman\\words.txt");
-		disallowedChars = game.Reader("C:\\Users\\etone\\Desktop\\javaProjects\\hangman\\disallowedChars.txt");
-		System.out.println(words);
-		System.out.println(disallowedChars);
+		words = game.Reader("C:\\Users\\etone\\Desktop\\javaProjects\\hangman\\res\\words.txt");
+		disallowedChars = game.Reader("C:\\Users\\etone\\Desktop\\javaProjects\\hangman\\res\\disallowedChars.txt");
+		//System.out.println(words);
+		//System.out.println(disallowedChars);
 
-		this.guessWord = targetWord;
+		this.targetWord = word;
+		targetWordLength = targetWord.length();
+		System.out.println(targetWord);
+		System.out.println(targetWordLength);
+
+		guesser.MakeGuessAlphabetical(targetWord, targetWordLength);
+
 	}
-
+	//Read a text file and store the content in an array
 	public List<String> Reader(String url){
 		List<String> words = new ArrayList<String>();
 		try{
+
 			File file = new File(url);
 			Scanner fileScanner = new Scanner(file);
 
 			while(fileScanner.hasNext()){
-				words.add(fileScanner.next());
+				words.add(fileScanner.next().toLowerCase());
 			}
 
-			fileScanner.close();	
+			fileScanner.close();
+
 		} catch(Exception e){
 
 		}
 
 		return words;
+
 	}
 	
 }
--- Guesser.java
+++ Guesser.java
@@ -3,8 +3,11 @@ import java.util.*;
 
 public class Guesser{
 
-	public static void main(String[] args) throws Exception{
+	public static void MakeGuessAlphabetical(String targetWord, int targetWordLength){
 
+		System.out.println("Guess being made");
+
+		
 	}
 
 }
